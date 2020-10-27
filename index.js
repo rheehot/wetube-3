9ada1bf312226bec76ecbd871f28873394f70194
민석 / MIN / ミンソク <55354735+minsoku@users.noreply.github.com>
2020-10-28 01:21:27 +0900

    callback function

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hi");
+const express = require("express");
+const app = express();
+
+const PORT = 10000;
+
+function handlelistening() {
+  console.log(`localhost:${PORT}접속`);
+}
+
+app.listen(PORT, handlelistening);
