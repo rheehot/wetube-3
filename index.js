ee183881876bcb1a613deaaba14f01a0079f4885
민석 / MIN / ミンソク <55354735+minsoku@users.noreply.github.com>
2020-10-29 00:05:28 +0900

    2.4, 2.5

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const express = require("express");
+import express from "express"
+
 const app = express();
 
 const PORT = 10000;
 
-function handlelistening() {
-  console.log(`localhost:${PORT}접속`);
-}
+const handlelistening = () => console.log(`localhost:${PORT}접속`);
+
+
+const handleHome = (req, res) => res.send('Hello from home')
+
+const handleProfile = (req, res) => res.send("you are on my profile")
+
+app.get("/", handleHome) 
+
+app.get("/profile", handleProfile)
 
 app.listen(PORT, handlelistening);
