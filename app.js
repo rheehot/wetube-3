07da2cb23adb3ce79d59d5f1444cbe7e69c00af2
민석 / MIN / ミンソク <55354735+minsoku@users.noreply.github.com>
2020-10-30 02:21:21 +0900

    2.9 MVC Pattern 과 router 폴더 생성 및 라우터 파일 생성

--- app.js
+++ app.js
@@ -3,25 +3,20 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import {userRouter} from "./router";
-
+import userRotuer from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 
 const app = express();
-
-const handleHome = (req, res) => res.send('Hello from home');
-
-const handleProfile = (req, res) => res.send("you are on my profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(helmet());
-app.use(morgan("tiny"));
+app.use(morgan("dev"));
 
-app.get("/",handleHome);
-
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter)
+app.use("/" , globalRouter);
+app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
 export default app;
---
+++ routers/videoRouter.js
@@ -0,0 +1,5 @@
+import express from "express"
+
+const vidoeRouter = express.Router();
+
+export default videoRouter;
