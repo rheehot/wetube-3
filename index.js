31bef97110ff62965c83983ff0342d6d6eec203f
민석 / MIN / ミンソク <55354735+minsoku@users.noreply.github.com>
2020-10-29 17:53:51 +0900

    2.7 middleware 2 morgan, helmet, body-parser, cookie-parser

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-import express from "express"
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = express();
 
@@ -7,19 +11,22 @@ const PORT = 10000;
 const handlelistening = () => console.log(`localhost:${PORT}접속`);
 
 
-const handleHome = (req, res) => res.send('Hello from home')
+const handleHome = (req, res) => res.send('Hello from home');
 
-const handleProfile = (req, res) => res.send("you are on my profile")
+const handleProfile = (req, res) => res.send("you are on my profile");
 
 const betweenHome = (req, res, next) => {
-    console.log("I'm between")
+    console.log("I'm between");
     next();
 }
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(morgan("tiny"));
+app.use(helmet());
 
-app.use(betweenHome)
+app.get("/",handleHome);
 
-app.get("/",handleHome) 
-
-app.get("/profile", handleProfile)
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handlelistening);
