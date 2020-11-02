9943a535862f89d7654340a50d0c0982870fdc8a
민석 / MIN / ミンソク <55354735+minsoku@users.noreply.github.com>
2020-11-03 01:36:31 +0900

    2.17 템플릿 하나에만 변수 추가하기 등등

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,7 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.send("Search");
-export const videos = (req, res) => res.send("videos");
-export const upload = (req, res) => res.send("upload");
-export const videoDetail = (req, res) => res.send("videoDetail");
-export const editVideo = (req, res) => res.send("editVideo");
-export const deleteVideo = (req, res) => res.send("deleteVideo");
+export const home = (req, res) => res.render("home", {pageTitle : "home"});
+export const search = (req, res) => res.render("Search", {pageTitle: "search"});
+export const videos = (req, res) => res.render("videos", {pageTitle: "videos"});
+export const upload = (req, res) => res.render("upload", {pageTitle: "upload"});
+export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle: "videoDetail"});
+export const editVideo = (req, res) => res.render("editVideo", {pageTitle: "editVideo"});
+export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle: "deleteVideo"});
