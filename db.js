2fa91d5300a0af00b405cba0d7e9632f668a7b65
민석 / MIN / ミンソク <55354735+minsoku@users.noreply.github.com>
2020-11-06 23:32:13 +0900

    2.21 Make fake db and connect

---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos = [
+    {
+        id: 11,
+        title : "grape",
+        description : 'melon',
+        view : 1111,
+        videoFile : "https://archive.org/details/BigBuckBunny_124",
+        creator : {
+            id : 111,
+            name : "min",
+            email : "min@m.com"
+        }
+    },
+    {
+        id: 12,
+        title : 'apple',
+        description : 'banana',
+        view : 2222,
+        videoFile : "https://archive.org/details/BigBuckBunny_124",
+        creator : {
+            id : 112,
+            name : "minn",
+            email : "minnn@mn.com"
+        }
+    },
+    {
+        id: 13,
+        title : 'milk',
+        description : 'potato',
+        view : 3333,
+        videoFile : "https://archive.org/details/BigBuckBunny_124",
+        creator : {
+            id : 113,
+            name : "ming",
+            email : "ming@ming.com"
+        }
+    }
+];
