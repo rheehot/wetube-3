74d9690a1ae0cb63af47368c61a7ce24d3564929
민석 / MIN / ミンソク <55354735+minsoku@users.noreply.github.com>
2020-11-08 14:11:26 +0900

    2.22-2 동영상 컨트롤러만 나오고 재생 안되는 것 고침

--- app.js
+++ app.js
@@ -17,8 +17,10 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(morgan("dev"));
-app.use(helmet())
-
+app.use(
+    helmet({ contentSecurityPolicy: { directives: { defaultSrc: ["*"] } } })
+  );
+  
 app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
