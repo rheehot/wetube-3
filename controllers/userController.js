0b0447494ceffac7425ffafa7d590095f866dce4
민석 / MIN / ミンソク <55354735+minsoku@users.noreply.github.com>
2020-11-08 21:24:27 +0900

    2.24 Log in and User Profile Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,22 +2,27 @@ import routes from '../routes';
 
 export const getJoin = (req, res) => {
     res.render("join", {pageTitle: "Join"})
-
 };
 export const postJoin = (req, res) => {
     const {
         body: { name, email, password, password2 }
     } = req;
+    console.log(req.body);
     if(password !== password2) {
         res.status(400);
         res.render("join", { pageTitle: "Join"})
     } else {
         // To Do: Register User
         // To Do: Log user
-        res.redirect(routes.home)
+        res.redirect(routes.home);
     }
 };
-export const login = (req, res) => res.render("login", {pageTitle: "login"});
+export const getLogin = (req, res) => res.render("login", {pageTitle: "login"});
+
+export const postLogin = (req, res) => {
+    res.redirect(routes.home);
+}
+
 export const logout = (req, res) => res.render("logout", {pageTitle: "logout"});
 export const users = (req, res) => res.render('users', {pageTitle: "users"});
 export const userDetail = (req, res) => res.render('userDetail', {pageTitle: "userDetail"});
